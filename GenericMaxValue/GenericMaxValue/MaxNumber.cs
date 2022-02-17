78607f03cf6d6e544e017975ec91a74c5abe56c3
Sampada <sampadahakke102@gmail.com>
2022-02-17 17:13:53 +0530

    [Sampada] Adding float Max Value Problem

--- GenericMaxValue/GenericMaxValue/MaxNumber.cs
+++ GenericMaxValue/GenericMaxValue/MaxNumber.cs
@@ -18,5 +18,17 @@ namespace GenericMaxValue
             else
                 Console.WriteLine("Max Value is {0}", num3);
         }
+        
+        //Method Used To Find Float Max Number
+        public static void FindFloatMaxNum(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num2) >= 0)
+                Console.WriteLine("Max Value is {0}", num1);
+            else if (num2.CompareTo(num3) >= 0)
+                Console.WriteLine("Max Value is {0}", num2);
+            else
+                Console.WriteLine("Max Value is {0}", num3);
+        }
+        
     }
 }
